c563159a86251c696697eb248b9e243e548f14fb
parth-kakdiya-13 <parthrkakadiya@gmail.com>
2024-12-28 15:33:28 +0530

    update:some changes in backend

--- eCommerce-backend/api/index.js
+++ eCommerce-backend/api/index.js
@@ -2,20 +2,25 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser')
 const cors = require('cors');
-mongoose.set('debug', true);
-
 const productrout = require('./routes/product.route')
-
-
+mongoose.set('debug', true);
 
 const app = express();
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+
+const mongoURI = "mongodb+srv://parthrkakdiya:prk59595@cluster0.qx5ox.mongodb.net/product?retryWrites=true&w=majority"
+
+mongoose.connect(mongoURI)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('Could not connect to MongoDB:', err));
+
+
 const allowedOrigins = [
     'https://ecommerce-one-pi-90.vercel.app',
     'https://ecommerce-v36f.vercel.app',
     'https://ecommerce-w2el.vercel.app'
 ];
-
 app.use(cors({
     origin: function (origin, callback) {
         if (!origin || allowedOrigins.includes(origin)) {
@@ -30,26 +35,10 @@ app.use(cors({
 }));
 
 
-app.use(bodyParser.urlencoded({ extended: false }))
-
-
-const mongoURI = "mongodb+srv://parthrkakdiya:prk59595@cluster0.qx5ox.mongodb.net/product?retryWrites=true&w=majority"
-
-mongoose.connect(mongoURI)
-    .then(() => console.log('Connected to MongoDB'))
-    .catch(err => console.error('Could not connect to MongoDB:', err));
-
-
-
 app.get('/', (req, res) => {
     res.json("hello");
 })
-app.get('/addproduct', () => {
-    res.json("addproduct")
-})
-app.get('/shop', () => {
-    res.json("shop")
-})
+
 app.use('/', productrout)
 
 
